516c6e74c83a5f61bbc4a705165203665142ecfd
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-14 15:35:19 +0100

    DeveloperTests added and passed

--- src/main/java/techStaff/Developer.java
+++ src/main/java/techStaff/Developer.java
@@ -7,4 +7,14 @@ public class Developer extends Employee {
     public Developer(String name, String NINumber, int salary){
         super(name, NINumber, salary);
     }
+
+    @Override
+    public double raiseSalary() {
+        return super.raiseSalary();
+    }
+
+    @Override
+    public int payBonus() {
+        return super.payBonus();
+    }
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,5 +1,8 @@
 import org.junit.Before;
+import org.junit.Test;
 import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
 
 public class DeveloperTest {
 
@@ -10,5 +13,28 @@ public class DeveloperTest {
         developer = new Developer("Randy Marsh", "RM 3SP U8", 25000);
     }
 
-    
+    @Test
+    public void developerHasName(){
+        assertEquals("Randy Marsh", developer.getName());
+    }
+
+    @Test
+    public void developerHasNINumber(){
+        assertEquals("RM 3SP U8", developer.getNINumber());
+    }
+
+    @Test
+    public void developerHasSalary(){
+        assertEquals(25000, developer.getSalary());
+    }
+
+    @Test
+    public void developersSalaryCanBeRaised(){
+        assertEquals(50000, developer.raiseSalary(), 0);
+    }
+
+    @Test
+    public void developerCanGetBonus(){
+        assertEquals(250, developer.payBonus(), 0);
+    }
 }
