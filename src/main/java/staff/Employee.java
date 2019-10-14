d390a691d3b71a4591a746e888a47cb69fe59a07
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-14 14:21:36 +0100

    initial commit & setup

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,27 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String NINumber;
+    private int salary;
+
+    public Employee(String name, String NINumber, int salary){
+        this.name = name;
+        this.NINumber = NINumber;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getNINumber(){
+        return NINumber;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+}
